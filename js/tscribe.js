92b39a66d24ebe154b6d4aa6502ed7ed6521e672
Thilo Wiertz <twi@posteo.de>
2019-10-08 17:42:48 +0200

    simplify and refactor code

--- js/tscribe.js
+++ js/tscribe.js
@@ -1,116 +1,43 @@
-const createSnapshot = function () {
-    localStorage.tscribe = transcript.value;
-    btnRestore.disabled = false;
-    btnDelete.disabled = false;
-}
+//
+// INITIALIZE
+//
 
-const stopPlayback = function (autoRewind) {
-    audio.pause();
-    audio.currentTime = audio.currentTime - autoRewind;
-}
-const startPlayback = function () {
-    audio.play();
-}
-const forward = function (step) {
-    audio.currentTime = audio.currentTime + step;
-}
-const backward = function (step) {
-    audio.currentTime = audio.currentTime - step;
-}
-const timeStamp = function (time) {
-    hms = new Date(time * 1000).toISOString().substr(11, 8);
-    const stamp = ` #${hms}#`
-    return stamp;
-}
-const uploadFile = function (event) {
+// enable restore/delete snapshot buttons if local storage exists
+document.addEventListener('DOMContentLoaded', function (event) {
+    if (localStorage.tscribe) {
+        restoreSnapshot.disabled = false;
+        deleteSnapshot.disabled = false;
+    }
+})
+
+
+
+//
+// AUDIO CONTROL
+//
+
+// stepsize for audio control (backward/forward) in seconds
+const stepSize = 5
+
+const audioFile = document.getElementById('input-file');
+const audio = document.getElementById('audio');
+const playbackSpeed = document.getElementById('range-playback-speed');
+const autoRewind = document.getElementById('auto-rewind');
+
+// add source to audio if file is selected
+audioFile.addEventListener('change', function (event) {
     const audioFile = event.target.files[0];
     const audioSrc = URL.createObjectURL(audioFile);
     audio.setAttribute('src', audioSrc);
-}
-
-const btnRestore = document.getElementById('restore');
-const audio = document.getElementById('audio');
-const transcript = document.getElementById('transcript');
-const file = document.getElementById('file');
-const speed = document.getElementById('speed');
-const interviewer = document.getElementById('interviewer');
-const respondent = document.getElementById('respondent');
-const autoSwitch = document.getElementById('auto-switch');
-const autoSave = document.getElementById('auto-save');
-const btnDelete = document.getElementById('delete');
-const autoRewind = document.getElementById('auto-rewind');
-const btnExport = document.getElementById('export');
-
-let ignoreNextEnter = false;
-let speaker = respondent;
-const step = 5;
-
-btnExport.addEventListener('click', function (event) {
-    var textToWrite = transcript.value;
-    var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
-    var fileNameToSaveAs = "transcript.txt";
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "download file";
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
 });
 
-speed.addEventListener('change', function (event) {
+// change playback speed
+playbackSpeed.addEventListener('change', function (event) {
     newSpeed = 1 + (event.target.value * 0.25);
     audio.playbackRate = newSpeed
 });
 
-btnRestore.addEventListener('click', function (event) {
-    snapshot = localStorage.tscribe;
-    snapshotEnd = snapshot.substr(snapshot.length - 40, snapshot.length);
-    const message = 'Found a snapshot ending with the following content: \n' + '...' + snapshotEnd + '\n Restore snapshot and delete current transcript?'
-    const confirmRestore = confirm(message);
-    console.log(confirmRestore);
-    if (confirmRestore) {
-        transcript.value = localStorage.tscribe
-    }
-});
-
-btnDelete.addEventListener('click', function (event) {
-    message = 'Are you sure you want to delete all local snapshots?';
-    confirmDelete = confirm(message);
-    if (confirmDelete) {
-        localStorage.removeItem('tscribe');
-        btnRestore.disabled = true;
-        btnDelete.disabled = true;
-    }
-});
-
-file.addEventListener('change', uploadFile);
-
-transcript.addEventListener('keydown', function (event) {
-    if (event.keyCode != 13) {
-        ignoreNextEnter = false;
-    };
-    if (event.keyCode === 13 && !ignoreNextEnter) {
-        ignoreNextEnter = true;
-        text = transcript.value + timeStamp(audio.currentTime) + '\n';
-        if (autoSwitch.checked) {
-            if (!event.shiftKey) {
-                speaker = (speaker === interviewer) ? respondent : interviewer;
-            };
-            if (speaker.value.length > 1) {
-                text += speaker.value + ': ';
-            } else {
-                text += speaker === interviewer ? 'I: ' : 'R: ';
-            }
-        };
-        event.preventDefault();
-        event.stopPropagation();
-        transcript.value = text;
-        if (autoSave.checked) createSnapshot();
-    }
-})
-
+// monitor keyboard events for audio playback
 document.addEventListener('keydown', function (event) {
     // add 37 (backword) and 39 (forward)
     switch (event.keyCode) {
@@ -148,10 +75,97 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function (event) {
-    const snapshot = localStorage.tscribe;
-    if (snapshot) {
-        btnRestore.disabled = false;
-        btnDelete.disabled = false;
+
+
+//
+// TRANSCRIPTION
+//
+
+const transcript = document.getElementById('text-transcript');
+const interviewer = document.getElementById('input-interviewer');
+const respondent = document.getElementById('input-respondent');
+const autoSwitch = document.getElementById('cb-auto-switch');
+
+// init current speaker value
+let speaker = respondent;
+
+// create timestamp
+// TODO: add milliseconds (format: hh:mm:ss-x)
+const timeStamp = function (time) {
+    hms = new Date(time * 1000).toISOString().substr(11, 8);
+    const stamp = ` #${hms}#`
+    return stamp;
+}
+
+// monitor keyboard events in transcript window
+transcript.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        event.stopPropagation();
+        text = transcript.value + timeStamp(audio.currentTime) + '\n';
+        if (autoSwitch.checked) {
+            if (!event.shiftKey) {
+                speaker = (speaker === interviewer) ? respondent : interviewer;
+            };
+            text += speaker.value + ': ';
+        };
+        transcript.value = text;
+        if (autoSave.checked) createSnapshot();
     }
 })
+
+
+
+//
+// DATA STORAGE AND EXPORT
+//
+
+const autoSave = document.getElementById('cb-auto-save');
+const restoreSnapshot = document.getElementById('btn-restore-snapshot');
+const deleteSnapshot = document.getElementById('btn-delete-snapshot');
+const exportTranscript = document.getElementById('btn-export');
+
+// create snapshot in local browser storage (triggered on line break)
+const createSnapshot = function () {
+    localStorage.tscribe = transcript.value;
+    restoreSnapshot.disabled = false;
+    deleteSnapshot.disabled = false;
+}
+
+// restore snapshot from local browser storage
+restoreSnapshot.addEventListener('click', function (event) {
+    snapshot = localStorage.tscribe;
+    snapshotEnd = snapshot.substr(snapshot.length - 40, snapshot.length);
+    const message = `Found a snapshot ending with the following content: \n... ${snapshotEnd} Restoring the snapshot will delete current transcript. Continue?`;
+    const confirmRestore = confirm(message);
+    if (confirmRestore) {
+        transcript.value = localStorage.tscribe
+    }
+});
+
+// delete snapshot from local browser storage
+deleteSnapshot.addEventListener('click', function (event) {
+    message = 'Are you sure you want to delete all local snapshots?';
+    confirmDelete = confirm(message);
+    if (confirmDelete) {
+        localStorage.removeItem('tscribe');
+        restoreSnapshot.disabled = true;
+        deleteSnapshot.disabled = true;
+    }
+});
+
+// export transcript to txt file
+exportTranscript.addEventListener('click', function (event) {
+    var transcriptText = transcript.value;
+    var textBlob = new Blob([transcriptText], { type: 'text/plain' });
+
+    //create dummy link to download blob
+    var downloadLink = document.createElement("a");
+    downloadLink.download = "transcript.txt";
+    downloadLink.innerHTML = "download file";
+    downloadLink.href = window.URL.createObjectURL(textBlob);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+});
